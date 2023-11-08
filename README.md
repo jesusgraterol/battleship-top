b0bd73de24f2861521810e2121eeae7d35ddf14b
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2023-11-08 11:29:45 -0400

    Initalize the project's README.md file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# battleship-top
+# Battleship Project
+
+This repository implements [The Odin Project's](https://www.theodinproject.com/) Battleship 
+Project with Vanilla CSS, JavaScript, Jest & Webpack. For more information about this project, please visit:
+
+[https://www.theodinproject.com/lessons/node-path-javascript-battleship](https://www.theodinproject.com/lessons/node-path-javascript-battleship)
+
+A live demo of this solution can be accessed here:
+
+[https://jesusgraterol.github.io/battleship-top/](https://jesusgraterol.github.io/battleship-top/)
--- src/scripts/modules/ship/ship.spec.js
+++ src/scripts/modules/ship/ship.spec.js
@@ -1,18 +1,23 @@
-describe('makePoniesPink', () => {
-	beforeAll(() => {
-		/* Runs before all tests */
-	})
-	afterAll(() => {
-		/* Runs after all tests */
-	})
-	beforeEach(() => {
-		/* Runs before each test */
-	})
-	afterEach(() => {
-		/* Runs after each test */
-	})
-	
-	test('can calculate 2 plus 2', () => {
-		expect(2 + 2).toBe(4)
-	})
-})
+/* eslint-disable no-undef */
+
+describe('test suite template', () => {
+  beforeAll(() => {
+
+  });
+
+  afterAll(() => {
+
+  });
+
+  beforeEach(() => {
+
+  });
+
+  afterEach(() => {
+
+  });
+
+  test('can calculate 2 plus 2', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
